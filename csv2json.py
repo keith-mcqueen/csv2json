d8338e2e1a7992af024a9632e0cae93828da478c
Keith McQueen <keith.mcqueen@macweenies.com>
2014-12-31 12:48:07 -0700

    Implemented export to JSON.  Added arguments for the maximum number of rows to export.

--- csv2json.py
+++ csv2json.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import csv
 import sys
+import json
 
 import pypred
 
@@ -11,55 +12,69 @@ import pypred
 get_elements = lambda search_list, indices: [search_list[i] for i in indices]
 
 
-class ZipCodes:
+class Csv2Json:
     def __init__(self):
-        self.input_file_path = None
+        self.csv_file_path = None
+        self.json_file_path = None
         self.requested_fields = []
-        self.zip_code_field = 'zip'
+        self.pk_field = None
         self.predicate = None
+        self.num_rows = -1
 
-        self.available_fields = ()
-        self.export_indices = []
+        self.available_fields = []
         self.export_fields = []
+        self.export_obj = None
 
         self.parse_args()
 
     def parse_args(self):
         # create the argument parser
-        parser = argparse.ArgumentParser(description='This program will extract a subset of zip code data from an '
-                                                     'input file and export it to another file',
+        parser = argparse.ArgumentParser(description='This program will extract a subset of data from a CSV '
+                                                     'file and export it to a JSON file',
                                          add_help=True)
 
         # add an argument for the input file
-        parser.add_argument('--input-file',
-                            help='path to the zip code input file (should be in CSV format)',
+        parser.add_argument('-i', '--input',
+                            help='path to the CSV input file',
                             required=True,
                             action='store')
 
-        parser.add_argument('--fields',
+        parser.add_argument('-f', '--fields',
                             help='comma-separated list of fields to be exported',
                             required=False,
                             action='store')
 
-        parser.add_argument('--zip-code-field',
-                            help='the name of the field containing the zip code',
+        parser.add_argument('-p', '--pk-field',
+                            help='the name of the field containing the primary key value.  If supplied, the output '
+                                 'JSON will be keyed by this value',
                             required=False,
-                            action='store',
-                            default='zip')
+                            action='store')
 
-        parser.add_argument('--predicate',
+        parser.add_argument('-c', '--condition',
                             help='Boolean expression to filter out unwanted data (only matches will be exported)',
                             required=False,
+                            action='store')
+
+        parser.add_argument('-n', '--num-rows',
+                            help='the maximum number of rows to be exported',
+                            required=False,
+                            action='store',
+                            type=int,
+                            default=-1)
+
+        parser.add_argument('-o', '--output',
+                            help='path to output file',
+                            required=True,
                             action='store')
 
         # parse the arguments
         args = parser.parse_args()
 
         # get the input file path
-        self.input_file_path = args.input_file
-        if not os.path.exists(self.input_file_path):
-            raise Exception('File not found: {}'.format(self.input_file_path))
-        if os.path.isdir(self.input_file_path):
+        self.csv_file_path = args.input
+        if not os.path.exists(self.csv_file_path):
+            raise Exception('File not found: {}'.format(self.csv_file_path))
+        if os.path.isdir(self.csv_file_path):
             raise Exception('Input file must be an actual file, not a directory')
 
         # get the list of fields to export
@@ -67,66 +82,78 @@ class ZipCodes:
             self.requested_fields = args.fields.split(',')
 
         # get the name of the field containing the zip code
-        self.zip_code_field = args.zip_code_field
+        self.pk_field = args.pk_field
+        if self.pk_field is not None:
+            self.export_obj = {}
+        else:
+            self.export_obj = []
 
         # get the predicate if there is one
-        if args.predicate is not None:
-            self.predicate = pypred.Predicate(args.predicate)
+        if args.condition is not None:
+            self.predicate = pypred.Predicate(args.condition)
+
+        # get the maximum number of rows to export
+        self.num_rows = args.num_rows
+
+        # get the path to the output file
+        self.json_file_path = args.output
 
     def load(self):
-        print 'Reading Zip Codes from {}...'.format(self.input_file_path)
+        print 'Reading data from {}...'.format(self.csv_file_path)
 
-        print self.predicate.description()
-        print self.predicate.is_valid()
-
-        with open(self.input_file_path) as f:
+        with open(self.csv_file_path) as f:
             # process the first row as the header
-            self.process_header(f.readline())
+            self.process_header(f.readline().strip())
 
             # use a CSV Dictionary Reader for the rest of file
             reader = csv.DictReader(f, self.available_fields)
             try:
                 for row in reader:
-                    if self.predicate is not None and self.predicate.evaluate(row):
-                        self.process_row(row)
+                    if self.num_rows < 0 or self.num_rows > 0:
+                        if self.predicate is None or self.predicate.evaluate(row):
+                            self.num_rows -= 1
+                            self.add_row(row)
+                    else:
+                        break
             except csv.Error as e:
-                sys.exit('file %s, line %d: %s' % (self.input_file_path, reader.line_num, e))
+                sys.exit('file %s, line %d: %s' % (self.csv_file_path, reader.line_num, e))
 
     def process_header(self, line):
         self.available_fields = line.split(',')
         print 'Available fields: {}'.format(', '.join(self.available_fields))
 
-        if self.zip_code_field not in self.available_fields:
-            raise Exception('The required zip code field "{}" is not one of the available fields'.
-                            format(self.zip_code_field))
+        if self.pk_field and self.pk_field not in self.available_fields:
+            raise Exception('The required primary key field "{}" is not one of the available fields'.
+                            format(self.pk_field))
 
         if len(self.requested_fields) == 0:
             # export all fields
-            self.export_indices = range(len(self.available_fields))
+            self.export_fields = self.available_fields
             print 'All available fields will be exported'
         else:
-            for field in self.requested_fields:
-                if field in self.available_fields:
-                    self.export_indices.append(self.available_fields.index(field))
-                else:
-                    print 'Field "{}" is not available for export and will be ignored'.format(field)
+            self.export_fields = [f for f in self.requested_fields if f in self.available_fields]
 
-            if len(self.export_indices) > 0:
-                self.export_fields = get_elements(self.available_fields, self.export_indices)
-                print 'The following fields will be exported: {}'.format(', '.join(self.export_fields))
-            else:
+            if len(self.export_fields) == 0:
+                self.export_fields = self.available_fields
                 print 'All available fields will be exported'
+            else:
+                print 'The following fields will be exported: {}'.format(', '.join(self.export_fields))
 
-    def process_row(self, row):
-        zip_code_record = {k: row[k] for k in self.export_fields}
+    def add_row(self, row):
+        record = {k: row[k] for k in self.export_fields}
+        print record
 
-        print zip_code_record
+        if self.pk_field is not None:
+            self.export_obj[row[self.pk_field]] = record
+        else:
+            self.export_obj.append(record)
 
     def export(self):
-        pass
+        with open(self.json_file_path, 'w') as output:
+            json.dump(self.export_obj, output)
 
 
 if __name__ == '__main__':
-    zips = ZipCodes()
-    zips.load()
-    zips.export()
+    c2j = Csv2Json()
+    c2j.load()
+    c2j.export()
